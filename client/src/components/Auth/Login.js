1742aafbb9c0162562ba3cdbe623c78d139353ee
Julian West <julian@hologram.io>
2021-02-22 11:13:31 -0500

    Add context, protected route

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,38 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import OAUTH_CLIENT_ID from "../../ENV";
 import { GraphQLClient } from "graphql-request";
 import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 
-const ME_QUERY = `
-{
-  me {
-    _id
-    name
-    email
-    picture
-  }
-}
-`;
+import Context from "../../context";
+import { ME_QUERY } from "../../graphql/queries";
 
 const Login = ({ classes }) => {
-  const onSuccess = async googleUser => {
-    const idToken = googleUser.getAuthResponse().id_token;
-    const client = new GraphQLClient(
-      "http://localhost:4000/graphql",
-      { headers: { authorization: idToken } }
-    );
-    const data = await client.request(ME_QUERY);
-    console.log({data})
-  }
-  const onFailure = error => console.log(error);
+  const { dispatch } = useContext(Context);
 
-  return <GoogleLogin clientId={OAUTH_CLIENT_ID}
-                      onSuccess={onSuccess}
-                      onFailure={onFailure}
-                      isSignedIn={true}
-  />;
+  const onFailure = error => console.log("Error logging in:", error);
+
+  const onSuccess = async googleUser => {
+    try {
+      const idToken = googleUser.getAuthResponse().id_token;
+      const client = new GraphQLClient(
+        "http://localhost:4000/graphql",
+        { headers: { authorization: idToken } }
+      );
+      const { me } = await client.request(ME_QUERY);
+      dispatch({ type: "LOGIN_USER", payload: me });
+      dispatch({ type: "IS_LOGGED_IN", payload: googleUser.isSignedIn() })
+    } catch (error) {
+      onFailure(error);
+    }
+  }
+
+  return (
+    <div className={classes.root}>
+      <Typography
+        component="h1"
+        variant="h3"
+        gutterBottom
+        noWrap
+        style={{ color: "rgb(66, 133, 234)" }}
+      >
+        Welcome
+      </Typography>
+      <GoogleLogin clientId={OAUTH_CLIENT_ID}
+                   onSuccess={onSuccess}
+                   onFailure={onFailure}
+                   isSignedIn={true}
+                   theme="dark"
+                   buttonText="Login with Google"
+      />
+    </div>
+  );
 };
 
 const styles = {
